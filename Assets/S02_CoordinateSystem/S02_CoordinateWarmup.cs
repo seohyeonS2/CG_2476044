3051a80a4fa8d27942121a870152080cd8ec2e1d
SeoHyeon <shyeon16@ewha.ac.kr>
2026-09-03 15:02:00 +0900

    Initial commit

---
+++ Assets/S02_CoordinateSystem/S02_CoordinateWarmup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class S02_CoordinateWarmup : MonoBehaviour
+{
+    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private Vector3 targetRotationEuler;
+    [SerializeField] private Vector3 targetScale = Vector3.one;
+
+    void Update()
+    {
+        transform.position = targetPosition;
+        transform.rotation = Quaternion.Euler(targetRotationEuler);
+        transform.localScale = targetScale;
+    }
+}
